706f6401ef9ef4d122b138b953f6202690c541d8
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-07-03 13:57:51 -0500

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				focus: {
+					red: '#ED4A71',
+					blue: '#269AA9',
+					green: '#25D5A1',
+					yellow: '#FEC34C',
+				},
+				base: {
+					light: '#F4F4F7',
+					DEFAULT: '#A8A8A8',
+					dark: '#3E3E3E',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
